d0205be71df78518a2b24ea2083181e9fc0f5208
Nguyen Anh Kha (Eric) Tran <83027539+riczfe@users.noreply.github.com>
2024-04-22 11:35:52 +0700

    software licensing 2

--- lib/MPU9250/MPU9250.cpp
+++ lib/MPU9250/MPU9250.cpp
@@ -1,8 +1,6 @@
 /*
   MPU9250.cpp
-  
-  Copyright (c) 2017 Bolder Flight Systems
-  
+    
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
--- lib/MPU9250/MPU9250.h
+++ lib/MPU9250/MPU9250.h
@@ -1,10 +1,6 @@
 /*
   MPU9250.h
-  Brian R Taylor
-  brian.taylor@bolderflight.com
-
-  Copyright (c) 2017 Bolder Flight Systems
-
+  
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
